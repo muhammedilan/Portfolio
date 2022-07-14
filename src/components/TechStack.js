04528e368380c14186727f6b3e7ced47b2dfe3e3
Muhammed İlan <ridvanilan@outlook.com>
2022-07-14 14:27:07 +0300

    Update TechStack.js

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -18,16 +18,16 @@ import {
 } from "react-icons/si";
 const TechStack = () => {
   const techBtnList = [
-    <SiHtml5 className="mx-auto text-[#E44D26] w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiCss3 className="mx-auto text-[#264DE4] w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiJavascript className="mx-auto text-[#F7DF1E]  w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiReact className="mx-auto text-[#61DAFB]  w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiNextdotjs className="mx-auto text-black w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiSass className="mx-auto text-[#CD6799]  w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiTailwindcss className="mx-auto text-[#06B6D4] w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiRedux className="mx-auto text-[#764ABC]  w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiVercel className="mx-auto text-black w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
-    <SiNetlify className="mx-auto text-[#2ABCB9] w-[30px] h-[30px] md:w-[34px] md:h-[34px]" />,
+    <SiHtml5 className="mx-auto text-[#E44D26] w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiCss3 className="mx-auto text-[#264DE4] w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiJavascript className="mx-auto text-[#F7DF1E]  w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiReact className="mx-auto text-[#61DAFB]  w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiNextdotjs className="mx-auto text-black w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiSass className="mx-auto text-[#CD6799]  w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiTailwindcss className="mx-auto text-[#06B6D4] w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiRedux className="mx-auto text-[#764ABC]  w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiVercel className="mx-auto text-black w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
+    <SiNetlify className="mx-auto text-[#2ABCB9] w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />,
   ];
 
   return (
@@ -60,21 +60,21 @@ const TechStack = () => {
         </div>
         <div className="mt-[20px]">
           <div className="flex items-center cursor-pointer w-[50px] h-[50px] sm:w-[54px] sm:h-[54px] md:w-[64px] md:h-[64px] hover:scale-125 transition float-left mb-[12px] duration-200 bg-white mr-[12px] shadow-[0px_4px_40px_0px_rgba(9,9,12,0.2)] text-[#599636]   rounded-full">
-            <SiMongodb className="mx-auto w-[32px] h-[32px] md:w-[34px] md:h-[34px]" />
+            <SiMongodb className="mx-auto w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />
           </div>
           <div className="flex items-center cursor-pointer w-[50px] h-[50px] sm:w-[54px] sm:h-[54px] md:w-[64px] md:h-[64px] hover:scale-125 float-left transition mb-[12px] duration-200 bg-white mr-[12px] shadow-[0px_4px_40px_0px_rgba(9,9,12,0.2)] text-black   rounded-full">
-            <SiExpress className="mx-auto w-[32px] h-[32px] md:w-[34px] md:h-[34px]" />
+            <SiExpress className="mx-auto w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />
           </div>
 
           <div className="flex items-center cursor-pointer w-[50px] h-[50px] sm:w-[54px] sm:h-[54px] md:w-[64px] md:h-[64px] hover:scale-125 float-left transition mb-[12px] duration-200 bg-white mr-[12px] shadow-[0px_4px_40px_0px_rgba(9,9,12,0.2)] text-[#8CC84B]   rounded-full">
-            <SiNodedotjs className="mx-auto w-[32px] h-[32px] md:w-[34px] md:h-[34px]" />
+            <SiNodedotjs className="mx-auto w-[28px] h-[28px] md:w-[34px] md:h-[34px]" />
           </div>
           <div className="flex items-center cursor-pointer w-[50px] h-[50px] md:w-[64px] md:h-[64px] hover:scale-125 float-left mb-[12px] justify-center transition duration-300 bg-white mr-[12px] shadow-[0px_4px_40px_0px_rgba(9,9,12,0.2)] text-[#A259FF] rounded-full">
             <img
               alt="Cloudinary"
               loading="lazy"
               src={Cloudinary}
-              className="w-[32px] h-[24px] sm:w-[32px] sm:h-[26px]"
+              className="w-[28px] h-[24px] sm:w-[32px] sm:h-[26px]"
             />
           </div>
         </div>
